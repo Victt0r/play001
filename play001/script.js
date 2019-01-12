069092d89995e354de6038ed1f65fa48f418a388
UniBreakfast <unibreakfast1@gmail.com>
2019-01-12 13:13:18 +0200

    added profile page

--- play001/script.js
+++ play001/script.js
@@ -1,35 +1,8 @@
-'use strict';
 
-function request(type, url, cb, reportcb, falldata, fallcb) {
-  const r = new XMLHttpRequest();
-  r.open(type, url, true);
-  r.onload = () => {
-    if (cb) {
-      if (r.status >= 200 && r.status < 400) {
-        if (!r.response.startsWith('<?php')) {
-          if (r.response !== '') cb(r.response);
-          else cb();
-        }
-        else if (reportcb)
-          reportcb('php file content returned instead of php-response');
-      }
-      else {
-        if (reportcb) reportcb('request.status is ' + r.status);
-        falldata ? cb(falldata) : cb();
-      }
-    }
-    else if (fallcb) falldata ? fallcb(falldata) : fallcb();
-  }
-  r.onerror =
-    e => reportcb(type + ' request to '+ url + ' produced ' + e);
-  r.ontimeout =
-    () => reportcb(type + ' request to '+ url + ' timed out!');
-  r.send();
-}
 
 
 request('GET','style.css', data => {
-  let pre = document.createElement('pre')
+  const pre = document.createElement('pre')
   pre.innerText = data
   document.getElementById('body').appendChild(pre)
 })
